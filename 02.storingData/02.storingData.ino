47b34ddbe5a24be16e24169f88ccc3aa0fd978fb
TheWebsiteMan <126206539+TheWebsiteMan@users.noreply.github.com>
2023-08-09 11:56:17 +1000

    complete activity 2
    it is done

--- 02.storingData/02.storingData.ino
+++ 02.storingData/02.storingData.ino
@@ -1,10 +1,12 @@
 /*
   Author: 
-  Learning Intention: The students will learn about the different data types used in the C++ language and how they can be converted for storage or computation
+  Learning Intention: The students will learn about the different data types used in the C++ language and how they can be 
+  converted for storage or computation
   Success Criteria:
     1. I understand and can store variables in the datatypes string, interger, float, longs and boolean
     2. I know how to access more information about the different datatypes in the documentation
-    3. I Understand how (1, HIGH and true) and (0, LOW, false) all represent the same concept and can be used interchangeably in an INO sketch
+    3. I Understand how (1, HIGH and true) and (0, LOW, false) all represent the same concept and can be used interchangeably 
+    in an INO sketch
     4. I can convert an integer, float and boolean to a string and print it in to the serial monitor
     5. I understand the difference between an int and an unsigned int or a long or an unsigned long
     6. I understand the difference between a global variable and a function variable 
@@ -15,11 +17,22 @@
     https://www.arduino.cc/reference/en/#variables
     https://www.arduino.cc/reference/en/language/variables/data-types/stringobject/
 */
+String mystring = "Hello World";
+int antoid = 6;
+bool mybool = false;
+float driftwood = 3.14;
+long msonata = 3.141592653589793;
+unsigned int antoid2 = 5;
+static unsigned int steelantoid = 7;
 
 void setup() {
-
+  Serial.begin(9600);
+  Serial.println("this is 9600");
+  Serial.println("_______________");
 }
 
 void loop() {
-
+  mybool = true;
+  mystring = mybool;
+  Serial.println(String(msonata));
 }
