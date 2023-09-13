3f36edcad1c47b09be13be9881e73fcad8ad47e5
TheWebsiteMan <126206539+TheWebsiteMan@users.noreply.github.com>
2023-09-13 14:07:55 +1000

    Update functions.ino

--- 13.I2C/functions.ino
+++ 13.I2C/functions.ino
@@ -5,9 +5,12 @@ void updateOLED (float theTemp) {
   u8g2.setCursor(0, 10);              // Set cursor to first line
   u8g2.print("Hello World!");         // write something to the internal memory
   u8g2.setCursor(0, 20);              // Set cursor to first line
-  u8g2.print("The temperature is: ");         // write something to the internal memory
-  u8g2.setCursor(0, 30);
-  u8g2.print(theTemp);
-  u8g2.print("*C");
+  u8g2.print("The temperature is not: ");         // write something to the internal memory
+  u8g2.setCursor(0, 30);                          //10 PRINT "HELLO, WORLD"
+  u8g2.print(theTemp);                            //20 GOTO 10
+  u8g2.print("*C");                               //
   u8g2.sendBuffer();                  // transfer internal memory to the display
+
+float sampletemp() {
+  return mypresen.getTemperature();
 }
